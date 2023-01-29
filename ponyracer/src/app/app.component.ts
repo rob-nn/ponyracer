4773e6cb6f415eb163485eda928e89b6ca0bdaea
roberto.lima <roberto.lima@lojasrenner.com.br>
2023-01-29 17:35:35 -0300

    first component

--- ponyracer/src/app/app.component.ts
+++ ponyracer/src/app/app.component.ts
@@ -2,9 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ns-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>PonyRacer</h1>
+    <h2> {{ numberOfUsers }} users </h2>
+    <h2> Wellcome {{ user?.name }} </h2>
+    <ns-races></ns-races>`
 })
 export class AppComponent {
   title = 'ponyracer';
+  user : {name : string };
+  numberOfUsers: number = 144;
+  constructor(){
+    this.user = {name:""};
+    setTimeout(()=>this.user.name = "Roberto", 5000);
+  }
 }
